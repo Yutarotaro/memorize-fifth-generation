896cb851540514f00bce95771d6dc34d368c5dc7
Yutarotaro <yutaro@yutaroMacbookAir.local>
2022-05-02 18:20:21 +0900

    少し動作は怪しいが、ストップウォッチを実装完了

--- src/Play.js
+++ src/Play.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import CountDown from './countDown';
-import {Stopwatch} from 'hooked-react-stopwatch';
 import {Form, Stack, ButtonGroup, Dropdown , Button, Navbar, Fade} from 'react-bootstrap'
 import config from './config'; import display from './display';
 import Countdown from 'react-countdown';
@@ -16,14 +15,59 @@ const shuffle = ([...array]) => {
   }
   return array;
 }
+
 const question_array = shuffle(config.typing_data.member.fifth);
 
+function Stopwatch(props){
+	const [time, setTime] = useState(0);
+	const [start, setStart] = useState(true);
+	let end = false;
+
+	if(props.stop && !end && start){
+		setStart((start)=>{
+			end = true;
+			console.log('OK');
+			return false;
+		});
+	}
+
+	useEffect(() => {
+		let interval = null;
+
+		if(start){
+			interval = setInterval(()=>{
+				setTime(time => time + 10);
+				
+			}, 10);
+		}else{
+			clearInterval(interval);
+		}
+
+		return () => clearInterval(interval)
+	},[start]);
+
+	return (
+		<>
+		<span>{('00'+(Math.floor((time / 60000) % 60 ))).slice(-2)}:</span>
+		<span>{('00'+(Math.floor((time / 1000) % 60 ))).slice(-2)}:</span>
+		<span>{('00'+(Math.floor((time / 10) % 100 ))).slice(-2)}</span>
+		</>
+	);
+}
+
+
+
+
+
+
+
 function Play(props){
-	console.log(question_array);
 	const checked = props.checked;
 	const [gameCount, setGameCount] = useState(0);
 	const [open, setOpen] = useState(true);
 	const [correct, setCorrect] = useState(0);
+
+
 
 
 	function Type(props){
@@ -71,31 +115,34 @@ function Play(props){
 
 
 
-
 	const renderer = ({hours, minutes, seconds, completed}) => {
 		return (completed) ? <Fade in={open}><h2>total time</h2></Fade>:<h2 className='text-white'>total time</h2>;}
 
-	if(gameCount < config.typing_data.total){
-		return(
-			<div>
+	let is_end = (gameCount >= config.typing_data.total);
+
+	return(
+		<div>
+		<Stopwatch stop={is_end} />
+		{(!is_end)?
+		<>
 			<h3>{gameCount+1}/{config.typing_data.total}問</h3>
 			<img className="mb-2 mt-2" src={`${img_path}`} width='200px'/>
 			<br/><br/>
 			<Type className="mb-2" />
 			<br/>
-			</div>
-		);
-	}else{
-		return(
-			<div>
+			</>
+		:
+		<>	
 			<Countdown date={Date.now() + 1000} renderer={renderer} />
-						<Stack gap={2} className="col-md-5 mx-auto">
+			<Stack gap={2} className="col-md-5 mx-auto">
 			  <Button variant="secondary" href="/game">Retry</Button>
 			  <Button variant="outline-secondary" href="/">Home</Button>
 			</Stack>
-			</div>
-		);
-	}
+		</>
+		}
+	
+		</div>
+	);
 }
 
 
